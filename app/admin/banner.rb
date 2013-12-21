e31635cc717adf5f6cc22b899e7909f4d20ffce3
Sergio Sebastian Arriagada <sarriagada87@gmail.com>
2013-12-21 14:43:45 -0300

    implement banner

---
+++ app/admin/banner.rb
@@ -0,0 +1,4 @@
+ActiveAdmin.register Banner do
+
+  permit_params :title, :caption
+end
--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,5 +1,6 @@
 class SiteController < ApplicationController
   def index
+    @banners = Banner.all.limit(3)
     @recomended = Game.all.limit(8)
     @most_visited = Game.all
   end
